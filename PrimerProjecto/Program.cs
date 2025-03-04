a5286604840d194355dc0ea90370c48f3cac588c
Duber <gutierrezduber12@gmail.com>
2025-03-04 17:53:21 -0500

    Primer commit

---
+++ PrimerProjecto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProjecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Como te llamas?: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola " + nombre);
+            Console.ReadLine();
+
+        }
+    }
+}
