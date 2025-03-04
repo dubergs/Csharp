cfa1e49fecd1f57fd72c41de45885404be77be0d
Duber <gutierrezduber12@gmail.com>
2025-03-04 18:00:29 -0500

    Creacion de la suma de dos numeros

--- PrimerProjecto/Program.cs
+++ PrimerProjecto/Program.cs
@@ -10,10 +10,16 @@ namespace PrimerProjecto
     {
         static void Main(string[] args)
         {
-            Console.Write("Como te llamas?: ");
-            string nombre = Console.ReadLine();
+            Console.Write("Ingresa primer numero: ");
+            //Convertir tipo de dato directamente desde el input
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Hola " + nombre);
+            Console.Write("Ingresar segundo numero: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("La suma es: " + (num1 + num2));
             Console.ReadLine();
 
         }
