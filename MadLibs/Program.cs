0a9e1603f2052dc3861c66b6e0bbce90143ebb39
Duber <gutierrezduber12@gmail.com>
2025-03-05 10:04:20 -0500

    Creacion de MadLibs y calculadora

---
+++ MadLibs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLibs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Matrices
+            int[] MisNumeros = { 1, 2, 3, 4, 5 };
+            string[] Paises = { "Colombia", "Panama", "Venezuela" };
+
+            Console.WriteLine("La lista contiene los siguientes numeros: " + Paises[0]);
+            Console.ReadLine();
+
+
+            //MadLibs
+            string color, comida, pais;
+            Console.WriteLine("Ingresa el color");
+            color = Console.ReadLine();
+
+            Console.WriteLine("Ingresa la comida");
+            comida = Console.ReadLine();
+
+            Console.WriteLine("Ingresa el pais");
+            pais = Console.ReadLine();
+
+            Console.WriteLine("Mi color favorito es " + color);
+            Console.WriteLine("Mi comida favorita es " + comida);
+            Console.WriteLine("Mi pais favorito es " + pais);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ calculadora/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Calculadora de Cuatro Funciones!");
+            Console.ReadLine();
+
+
+            Console.Write("Ingresa el primer numero: ");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingresa el segundo numero: ");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("sumar, restar, multiplicar, dividir");
+            Console.Write("Que operacion deseas hacer?: ");
+            string operador = Console.ReadLine();
+
+            if (operador == "sumar")
+            {
+                Console.WriteLine("El resultado de la suma es: " + (numero1 + numero2));
+                Console.ReadLine();
+            }
+            else if(operador == "restar")
+            {
+                Console.WriteLine("El resultado de la resta es: " + (numero1 - numero2));
+                Console.ReadLine();
+            }
+            else if(operador == "multiplicar")
+            {
+                Console.WriteLine("El resultado de la multiplicacion es: " + (numero1 * numero2));
+                Console.ReadLine();
+            }
+            else if(operador == "dividir")
+            {
+                Console.WriteLine("El resultado de la division es: " + (numero1 / numero2));
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Lo siento no contamos con esta operacion");
+                Console.ReadLine();
+            }
+        }
+    }
+}
