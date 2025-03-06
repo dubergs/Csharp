fe7828ae9075d363ca8e067b1dc0d2d17c4e6e0c
Duber <gutierrezduber12@gmail.com>
2025-03-06 09:11:45 -0500

    Creacion de juego de adivinanza

--- PrimerProjecto/Program.cs
+++ PrimerProjecto/Program.cs
@@ -10,14 +10,44 @@ namespace PrimerProjecto
     {
         static void Main(string[] args)
         {
-            Console.Write("Ingresa primer numero: ");
-            //Convertir tipo de dato directamente desde el input 
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ingresa primer numero: ");
+            ////Convertir tipo de dato directamente desde el input 
+            //int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Ingresar segundo numero: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ingresar segundo numero: ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("La suma es: " + (num1 + num2));
+            //Console.ReadLine();
+
+            string palabraSecreta = "Lunes";
+            string respuesta = "";
+            int cuentaTotal = 0;
+            int limiteTotal = 5;
+            bool auto = false;
             
-            Console.WriteLine("La suma es: " + (num1 + num2));
+            while (respuesta != palabraSecreta && !auto)
+            {
+                if (cuentaTotal < limiteTotal) 
+                {
+                    Console.Write("Ingresa tu palabra aqui: ");
+                    respuesta = Console.ReadLine();
+                    cuentaTotal++;
+                }
+                else
+                {
+                    auto = true;
+                }
+                
+            }
+            if (auto)
+            {
+                Console.WriteLine("Perdistes!");
+            }
+            else
+            {
+                Console.WriteLine("Ganaste!");
+            }
             Console.ReadLine();
         }
     }
